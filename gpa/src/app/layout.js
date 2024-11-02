3728e3f4ca51ed737ca748600e6446b6ce8c62d5
Rudy <80164496+realrudy@users.noreply.github.com>
2024-11-02 10:04:04 -0700

    yay

---
+++ gpa/src/app/layout.js
@@ -0,0 +1,26 @@
+import localFont from "next/font/local";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import Classes from "../app/classes";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400','600','700','800']
+})
+
+export const metadata = {
+  title: "GPA Calculator",
+  description: "Calculate GPA",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ gpa/src/app/page.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Classes from "../app/classes";
+import Button from "../app/button";
+
+export default function Home() {
+  
+
+  return (
+    <div>
+      <div className="relative isolate overflow-hidden bg-gray-300 py-24 sm:py-32">
+      <h1 className="text-4xl ml-10 text-gray-700 font-semibold">GPA Calculator</h1>
+      <h2 className="text-md ml-10">Calculate your GPA below! Quick and easy.</h2>
+
+    </div>
+    <div className="relative isolate overflow-hidden py-5 p-10">
+
+
+    <h3 className="text-xl">Class Name / Grades</h3>
+    <Classes />
+ 
+    </div>
+    <div className="ml-10 mb-10 w-1/6" >
+    <Button />
+
+</div>
+    </div>
+  );
+}
